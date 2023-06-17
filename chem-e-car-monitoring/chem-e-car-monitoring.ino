eb7233dc2f8b4b8ecbb7686f721dc92cabc62766
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-17 20:27:34 +0700

    feat: can now send MPU data

--- chem-e-car-monitoring/chem-e-car-monitoring.ino
+++ chem-e-car-monitoring/chem-e-car-monitoring.ino
@@ -2,21 +2,16 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include "indexPage.h"
 #include "aboutPage.h"
 #include "settingsPage.h"
 #include "DHT.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
 // WiFi SSID and Password
 const char *ssid = "IOT-ZAKI";
 const char *password = "44444444";
-
-// OLED setup
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 // Web Server and Web Socket setup
 AsyncWebServer server(80);
@@ -27,34 +22,34 @@ AsyncWebSocket ws("/ws");
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
+// MPU setup
+Adafruit_MPU6050 mpu;
+float originalHeading = 0.0;
+
 void setup() {
   Serial.begin(115200);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
+
   // WiFi connection
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-  // Display the IP Address
-  Serial.println(WiFi.localIP());
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  display.println("IP Address: ");
-  display.println(WiFi.localIP());
-  display.display();
+
   // Start server and web socket
   server.begin();
   server.addHandler(&ws);
 
   // DHT
   dht.begin();
+
+  // MPU
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
 
   // Route: "/"
   // Method: GET
@@ -76,9 +71,13 @@ void setup() {
 void loop() {
     ws.cleanupClients();
 
-    // Test random data
+    // DHT Data
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
+    // MPU Data
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    float wrongHeading = originalHeading + g.gyro.z;
 
     // Constructing JSON
     String jsonString = "";
@@ -86,7 +85,23 @@ void loop() {
     jsonString += temperature;
     jsonString += ", \"humidity\" : ";
     jsonString += humidity;
+    jsonString += ", \"accelerationX\" : ";
+    jsonString += a.acceleration.x;
+    jsonString += ", \"accelerationY\" : ";
+    jsonString += a.acceleration.y;
+    jsonString += ", \"accelerationZ\" : ";
+    jsonString += a.acceleration.z;
+    jsonString += ", \"rotationX\" : ";
+    jsonString += g.gyro.x;
+    jsonString += ", \"rotationY\" : ";
+    jsonString += g.gyro.y;
+    jsonString += ", \"rotationZ\" : ";
+    jsonString += g.gyro.z;
+    jsonString += ", \"missAngle\" : ";
+    jsonString += wrongHeading;
     jsonString += "}";
+
+    originalHeading = wrongHeading;
 
     // Sending JSON using websocket
     ws.textAll(jsonString);
