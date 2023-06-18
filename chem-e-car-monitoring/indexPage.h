f37241e9778fe160f40ac4f13f8961a14a693739
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-18 08:27:29 +0700

    feat: update index page

--- chem-e-car-monitoring/indexPage.h
+++ chem-e-car-monitoring/indexPage.h
@@ -31,7 +31,9 @@ String indexPage = R"HTML(
             id="active-indicator"
             class="absolute right-0 top-0 bg-[#F5F5F5] w-[95%] h-[33%] rounded-l-full"
           ></div>
-          <li class="py-3 z-[2] font-semibold text-transparent bg-gradient-purple2 bg-clip-text">
+          <li
+            class="py-3 z-[2] font-semibold text-transparent bg-gradient-purple2 bg-clip-text"
+          >
             <a href="/" class="flex items-center gap-3">
               <img
                 class="h-[28px] mt-[-3px]"
@@ -82,7 +84,7 @@ String indexPage = R"HTML(
               </h1>
               <h2
                 id="temperature"
-                class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
               >
                 0 °C
               </h2>
@@ -102,7 +104,7 @@ String indexPage = R"HTML(
               </h1>
               <h2
                 id="humidity"
-                class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
               >
                 0 %
               </h2>
@@ -113,23 +115,23 @@ String indexPage = R"HTML(
               />
             </div>
           </div>
-          <div class="flex gap-5 flex-col lg:flex-row">
+          <div
+            class="drop-shadow-1 bg-white px-4 py-3 relative overflow-hidden rounded-[20px] w-[550px] container h-min"
+          >
             <div
-              class="drop-shadow-1 bg-white px-4 py-3 relative overflow-hidden rounded-[20px] w-[550px] container h-min"
+              class="flex justify-between mb-3 w-full lg:w-1/2 flex-shrink-0"
             >
-              <div
-                class="flex justify-between mb-3 w-full lg:w-1/2 flex-shrink-0"
+              <h1
+                class="text-[20px] text-transparent bg-gradient-purple2 bg-clip-text font-semibold"
               >
-                <h1
-                  class="text-[20px] text-transparent bg-gradient-purple2 bg-clip-text font-semibold"
-                >
-                  Status Chart
-                </h1>
-              </div>
-              <div>
-                <canvas id="myChart"></canvas>
-              </div>
+                Status Chart
+              </h1>
             </div>
+            <div>
+              <canvas id="DHTChart"></canvas>
+            </div>
+          </div>
+          <div class="flex gap-5">
             <div
               class="drop-shadow-1 bg-white px-4 py-3 rounded-[20px] container w-fit"
             >
@@ -148,89 +150,133 @@ String indexPage = R"HTML(
                   Monitoring
                 </div>
               </div>
-              <div class="flex gap-10">
-                <div class="flex flex-col gap-5">
+              <div class="flex flex-col gap-10">
+                <div class="grid grid-cols-3 gap-5">
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
-                      AccelerationX (m/s<sup>2</sup>)
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
+                      Accel. X (m/s<sup>2</sup>)
                     </h1>
                     <h2
                       id="accelerationX"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
-                      AccelerationY (m/s<sup>2</sup>)
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
+                      Accel. Y (m/s<sup>2</sup>)
                     </h1>
                     <h2
                       id="accelerationY"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
-                      AccelerationZ (m/s<sup>2</sup>)
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
+                      Accel. Z (m/s<sup>2</sup>)
                     </h1>
                     <h2
                       id="accelerationZ"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
-                      Misalignment (°)
-                    </h1>
-                    <h2
-                      id="misalignmentAngle"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
                     >
-                      0
-                    </h2>
-                  </div>
-                </div>
-                <div class="flex flex-col gap-4">
-                  <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
                       Roll Drift (°)
                     </h1>
                     <h2
                       id="rollDrift"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
                       Pitch Drift (°)
                     </h1>
                     <h2
                       id="pitchDrift"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                   <div>
-                    <h1 class="text-[#828282] text-[14px] font-semibold">
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
                       Yaw Drift (°)
                     </h1>
                     <h2
                       id="yawDrift"
-                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-fit"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
+                    >
+                      0
+                    </h2>
+                  </div>
+                  <div>
+                    <h1
+                      class="text-[#828282] text-center text-[14px] font-semibold"
+                    >
+                      Misalignment (°)
+                    </h1>
+                    <h2
+                      id="misalignmentAngle"
+                      class="text-[30px] font-bold text-transparent bg-gradient-purple2 bg-clip-text w-full text-center"
                     >
                       0
                     </h2>
                   </div>
                 </div>
               </div>
+            </div>
+            <div
+              class="drop-shadow-1 bg-white px-4 py-3 relative overflow-hidden rounded-[20px] w-[550px] container h-min"
+            >
+              <div
+                class="flex justify-between mb-3 w-full lg:w-1/2 flex-shrink-0"
+              >
+                <h1
+                  class="text-[20px] text-transparent bg-gradient-purple2 bg-clip-text font-semibold"
+                >
+                  Acceleration Chart
+                </h1>
+              </div>
+              <div>
+                <canvas id="AccelerationChart"></canvas>
+              </div>
+            </div>
+          </div>
+          <div
+            class="drop-shadow-1 bg-white px-4 py-3 relative overflow-hidden rounded-[20px] w-[550px] container h-min"
+          >
+            <div
+              class="flex justify-between mb-3 w-full lg:w-1/2 flex-shrink-0"
+            >
+              <h1
+                class="text-[20px] text-transparent bg-gradient-purple2 bg-clip-text font-semibold"
+              >
+                Gyro Chart
+              </h1>
+            </div>
+            <div>
+              <canvas id="GyroChart"></canvas>
             </div>
           </div>
         </main>
@@ -288,7 +334,6 @@ String indexPage = R"HTML(
     }
   </style>
   <script defer>
-    const ctx = document.getElementById("myChart");
     const body = document.querySelector("body");
     const espIP = window.location.host;
     console.log(espIP);
@@ -301,12 +346,13 @@ String indexPage = R"HTML(
       document
         .querySelector("#active-indicator")
         .classList.add("indicator-dark");
-    }
+      }
 
+    const DHTChart = document.getElementById("DHTChart");
     const temperatures = [];
     const humidities = [];
     const labels = [];
-    const chart = new Chart(ctx, {
+    const chart1 = new Chart(DHTChart, {
       type: "line",
       data: {
         labels: labels,
@@ -351,11 +397,123 @@ String indexPage = R"HTML(
       },
     });
 
+    const MPUChart = document.getElementById("AccelerationChart");
+    const accelXs = [];
+    const accelYs = [];
+    const accelZs = [];
+    const chart2 = new Chart(MPUChart, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Accel X (m/s²)",
+            data: accelXs,
+            borderWidth: 1,
+            backgroundColor: "#5740B4",
+            borderColor: "#5740B4",
+          },
+          {
+            label: "Accel Y (m/s²)",
+            data: accelYs,
+            borderWidth: 1,
+            backgroundColor: "#2A58D0",
+            borderColor: "#2A58D0",
+          },
+          {
+            label: "Accel Z (m/s²)",
+            data: accelZs,
+            borderWidth: 1,
+            backgroundColor: "rgba(66, 145, 180)",
+            borderColor: "rgba(66, 145, 180)"
+          }
+        ],
+      },
+      options: {
+        borderWidth: 10,
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              color:
+                localStorage.getItem("theme") == "dark"
+                  ? "#f5f5f522"
+                  : "#16181B44",
+            },
+          },
+          x: {
+            grid: {
+              color:
+                localStorage.getItem("theme") == "dark"
+                  ? "#f5f5f522"
+                  : "#16181B44",
+            },
+          },
+        },
+      },
+    });
+
+    const GyroChart = document.getElementById("GyroChart");
+    const gyroXs = [];
+    const gyroYs = [];
+    const gyroZs = [];
+    const chart3 = new Chart(GyroChart, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Gyro X (°/s)",
+            data: gyroXs,
+            borderWidth: 1,
+            backgroundColor: "#5740B4",
+            borderColor: "#5740B4",
+          },
+          {
+            label: "Gyro Y (°/s)",
+            data: gyroYs,
+            borderWidth: 1,
+            backgroundColor: "#2A58D0",
+            borderColor: "#2A58D0",
+          },
+          {
+            label: "Gyro Z (°/s)",
+            data: gyroZs,
+            borderWidth: 1,
+            backgroundColor: "rgba(66, 145, 180)",
+            borderColor: "rgba(66, 145, 180)"
+          }
+        ],
+      },
+      options: {
+        borderWidth: 10,
+        scales: {
+          y: {
+            beginAtZero: true,
+            grid: {
+              color:
+                localStorage.getItem("theme") == "dark"
+                  ? "#f5f5f522"
+                  : "#16181B44",
+            },
+          },
+          x: {
+            grid: {
+              color:
+                localStorage.getItem("theme") == "dark"
+                  ? "#f5f5f522"
+                  : "#16181B44",
+            },
+          },
+        },
+      },
+    });
+    
     const websocket = new WebSocket(`ws://${espIP}/ws`);
     websocket.addEventListener("open", (event) => {
       websocket.send("Hello Server!");
     });
-
+    
     websocket.addEventListener("message", (event) => {
       const temperature = document.querySelector("#temperature");
       temperature.innerHTML = JSON.parse(event.data).temperature + " °C";
@@ -379,31 +537,66 @@ String indexPage = R"HTML(
         }:${now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds()}`
       );
 
-      chart.data.datasets[0].data = temperatures;
-      chart.data.labels = labels;
-      chart.update();
-
       const accelerationX = document.querySelector("#accelerationX");
       accelerationX.innerHTML = JSON.parse(event.data).accelerationX;
+      if (accelXs.length > 10) {
+        accelXs.shift();
+      }
+      accelXs.push(JSON.parse(event.data).accelerationX);
 
       const accelerationY = document.querySelector("#accelerationY");
       accelerationY.innerHTML = JSON.parse(event.data).accelerationY;
+      if (accelYs.length > 10) {
+        accelYs.shift();
+      }
+      accelYs.push(JSON.parse(event.data).accelerationY);
 
       const accelerationZ = document.querySelector("#accelerationZ");
       accelerationZ.innerHTML = JSON.parse(event.data).accelerationZ;
+      if (accelZs.length > 10) {
+        accelZs.shift();
+      }
+      accelZs.push(JSON.parse(event.data).accelerationZ);
 
       const pitch = document.querySelector("#pitchDrift");
       pitch.innerHTML = JSON.parse(event.data).rotationY;
+      if(gyroYs.length > 10) {
+        gyroYs.shift();
+      }
+      gyroYs.push(JSON.parse(event.data).rotationY);
 
       const roll = document.querySelector("#rollDrift");
       roll.innerHTML = JSON.parse(event.data).rotationX;
+      if(gyroXs.length > 10) {
+        gyroXs.shift();
+      }
+      gyroXs.push(JSON.parse(event.data).rotationX);
 
       const yaw = document.querySelector("#yawDrift");
       yaw.innerHTML = JSON.parse(event.data).rotationZ;
+      if(gyroZs.length > 10) {
+        gyroZs.shift();
+      }
+      gyroZs.push(JSON.parse(event.data).rotationZ);
 
       const misalignmentAngle = document.querySelector("#misalignmentAngle");
-      misalignmentAngle.innerHTML =
-        JSON.parse(event.data).missAngle;
+      misalignmentAngle.innerHTML = JSON.parse(event.data).missAngle;
+
+      chart1.data.datasets[0].data = temperatures;
+      chart1.data.labels = labels;
+      chart1.update();
+
+      chart2.data.datasets[0].data = accelXs;
+      chart2.data.datasets[1].data = accelYs;
+      chart2.data.datasets[2].data = accelZs;
+      chart2.data.labels = labels;
+      chart2.update();
+
+      chart3.data.datasets[0].data = gyroXs;
+      chart3.data.datasets[1].data = gyroYs;
+      chart3.data.datasets[2].data = gyroZs;
+      chart3.data.labels = labels;
+      chart3.update();
     });
   </script>
 </html>
